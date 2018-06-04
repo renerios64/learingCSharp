ebe5a292564dc6fefadc61262268bb5c229473c1
Rene <rios.1213@gmail.com>
2018-06-03 20:46:14 -0500

    Doing Enum Stuff

--- LearningAboutMockTests/TestLearningEnum.cs
+++ LearningAboutMockTests/TestLearningEnum.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
 
 namespace LearningAboutMockTests
 {
+    enum DaysOfTheWeek {
+        Sunday = 1,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thrusday,
+        Friday,
+        Saturday
+    }
+
+    enum DumbDaysOfTheWeek {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thrusday,
+        Friday,
+        Saturday
+    }
+
     class TestLearningEnum
     {
+        [Test]
+        public void CanAccessEnum()
+        {
+            //Assert.AreEqual("Sunday", DaysOfTheWeek.Sunday);
+            Assert.AreEqual(5, (int)DaysOfTheWeek.Thrusday);
+
+            Assert.AreEqual(0, (int)DumbDaysOfTheWeek.Sunday);
+        }
     }
 }
