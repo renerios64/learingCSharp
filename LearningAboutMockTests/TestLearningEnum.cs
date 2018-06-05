df9ffe7a1b7ebeefc19008c73b1b7d810a1b7122
Rene <rios.1213@gmail.com>
2018-06-04 23:14:36 -0500

    Adding another tests for clarification

--- LearningAboutMockTests/TestLearningEnum.cs
+++ LearningAboutMockTests/TestLearningEnum.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace LearningAboutMockTests
 {
@@ -31,6 +32,33 @@ namespace LearningAboutMockTests
             Assert.AreEqual(5, (int)DaysOfTheWeek.Thrusday);
 
             Assert.AreEqual(0, (int)DumbDaysOfTheWeek.Sunday);
+
+            var a = DaysOfTheWeek.Friday;
+            if (a == DaysOfTheWeek.Friday)
+            {
+                Assert.AreEqual(a, DaysOfTheWeek.Friday);
+            }
+        }
+
+        [Test]
+        public void WhatEnumsTryToSolve()
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("Sunday", 1);
+            dictionary.Add("Monday", 2);
+            dictionary.Add("Tuesday", 3);
+            dictionary.Add("Wednesday", 4);
+            dictionary.Add("Thursday", 5);
+            dictionary.Add("Friday", 6);
+            dictionary.Add("Saturday", 7);
+
+            var a = dictionary["Friday"];
+
+            if (a == 6)
+            {
+                Assert.AreEqual(a, 6);
+            }
+
         }
     }
 }
